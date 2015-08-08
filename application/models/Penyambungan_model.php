ac786b44eea1981d646b3340a9841218aca5a8df
Agung Setiawan <com.agungsetiawan@gmail.com>
2015-08-08 15:22:10 +0700

    Kembalikan count yang tidak sengaka terhapus

--- application/models/Penyambungan_model.php
+++ application/models/Penyambungan_model.php
@@ -12,6 +12,13 @@ class Penyambungan_model extends CI_Model {
         $this->db->insert($this->table, $data);
     }
 
+    public function count()
+    {
+       $this->db->not_like('tanggal_cabut', '0000-00-00');
+       $this->db->from($this->table);
+       return $this->db->count_all_results(); 
+    }
+
     public function get_by_idpelanggan($idPelanggan){
         $this->db->select('penyambungan_sementara.id,nama, alamat,no_telepon,daya,tarif,rpkwh,tujuan,tanggal_pasang,stand_awal,petugas_pasang,id_kwh_ganti,tanggal_cabut');
         $this->db->where('penyambungan_sementara.id_pelanggan', $idPelanggan);
